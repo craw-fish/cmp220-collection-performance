faa1239f8eef5396d6b853dbcac689b868086099
craw-fish <69093019+craw-fish@users.noreply.github.com>
2025-02-21 15:50:32 -0500

    average results from multiple multiple benchmarks
    Each benchmark is now run multiple times, then averaged together for final results. Reduced variance in runtime values.

--- src_python/collections_demo.py
+++ src_python/collections_demo.py
@@ -4,14 +4,22 @@ import collection_utils
 importlib.reload(collection_utils)
 from collection_utils import make_collections, collectionTest
 
-results = pd.DataFrame(columns=['length', 'benchmark', 'array', 'list', 'deque'])
-# multi-index: benchmark outside, length inside (for each benchmark, show results of each length)
-results.set_index(['benchmark', 'length'], inplace=True)
+# perform benchmarks multiple times, compile results into list of df's
+all_results = []
+for i in range(3):
+    results_i = pd.DataFrame(columns=['length', 'benchmark', 'array', 'list', 'deque'])
+    # multi-index: benchmark outside, length inside (for each benchmark, show results of each length)
+    results_i.set_index(['benchmark', 'length'], inplace=True)
 
-make_collections(max_length=10**7, scale=2)
+    make_collections(max_length=10**6, scale=10)
 
-# fill df with results
-for test in collectionTest.tests:
-    results.loc[('populate', test.length), test.type] = test.runtimes['populate']
+    # fill df with results
+    for test in collectionTest.tests:
+        results_i.loc[('populate', test.length), test.type] = test.runtimes['populate']
 
-print(results)
+    results_i = results_i.unstack(level=1).stack(future_stack=True)
+    all_results.append(results_i)
+
+# average of all dataframes
+avg_results = pd.concat(all_results).groupby(level=['benchmark', 'length']).mean()
+avg_results

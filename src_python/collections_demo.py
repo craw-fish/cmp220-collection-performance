eabc454a7c06690aec46a8549ae2092a2ebd7b14
craw-fish <69093019+craw-fish@users.noreply.github.com>
2025-02-24 17:26:04 -0500

    more verbose output dialog

--- src_python/collections_demo.py
+++ src_python/collections_demo.py
@@ -19,13 +19,17 @@ total_start = timeit.default_timer()
 n_trials = 3
 all_results = []
 for i in range(n_trials):
+    print(f"Running trial {i+1} of {n_trials}...")
     results_i = pd.DataFrame(columns=['benchmark', 'length_log', 'array', 'list', 'deque'])
     # multi-index: benchmark outside, length inside (for each benchmark, show results of each length)
     results_i.set_index(['benchmark', 'length_log'], inplace=True)
     
     # instantiate collections; also records 'populate' runtime
+    print("\tPopulating collections...")
     make_collections(max_length=10**6, scale=10)
-
+    print("\tCollections populated.")
+    
+    print("\tRunning benchmarks...")
     for test in collectionTest.tests:
         # run benchmarks
         sort_items(test)
@@ -34,6 +38,7 @@ for i in range(n_trials):
         # add runtime results to df
         for benchmark, runtime in test.runtimes.items():
             results_i.loc[(benchmark, np.log10(test.length)), test.type] = runtime
+    print("\tBenchmarks completed.")
     
     # empty list of collection tests
     collectionTest.tests.clear()      
@@ -41,12 +46,11 @@ for i in range(n_trials):
     results_i = results_i.unstack(level=1).stack(future_stack=True)
     all_results.append(results_i)
 
+total_end = timeit.default_timer()
+print("All trials completed.")
+print(f"Total runtime: {total_end - total_start:.3f}s ({1000 * (total_end - total_start):.0f}ms)")
+
 # average of all generated dataframes
 avg_results = pd.concat(all_results).groupby(level=['benchmark', 'length_log'], sort=False).mean()
-print(avg_results)
-
-total_end = timeit.default_timer()
-print(f"\nRuntimes averaged across {n_trials} trial(s)."
-      f"\nTotal runtime: {total_end - total_start:.3f}s ({1000 * (total_end - total_start):.0f}ms)")
 
 graph_results(avg_results)
